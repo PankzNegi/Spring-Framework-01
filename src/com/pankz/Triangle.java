7c73cefec8c2071fcecccebc679a32b4b8efcd73
Pankz <pankznegi4638@gmail.com>
2023-03-21 12:51:05 +0530

    Constructor Injection

--- src/com/pankz/Triangle.java
+++ src/com/pankz/Triangle.java
@@ -2,20 +2,47 @@ package com.pankz;
 
 public class Triangle {
 	private String type;
+	private int height;
 	
+	
+
+	public Triangle(String type) {
+		super();
+		this.type = type;
+	}
+	
+	
+
+	public Triangle(int height) {
+		super();
+		this.height = height;
+	}
+
+
+
+	public Triangle(String type, int height) {
+		super();
+		this.type = type;
+		this.height = height;
+	}
+
+
 	public String getType() {
 		return type;
 	}
 
-	public void setType(String type) {
+	public int getHeight() {
+		return height;
+	}
+	/*public void setType(String type) {
 		this.type = type;
 	}
-
+*/
 	public void draw()
 	{
 		
 		
-		System.out.println(getType()+" Triangle drawn");
+		System.out.println(getType()+" Triangle drawn of height "+getHeight());
 	}
 
 }
